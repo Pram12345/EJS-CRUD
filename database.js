efb190bad1858b8f23b289d141551615ae6a1c7b
Gulshan kumar <121786265+Gulshanray@users.noreply.github.com>
2024-02-01 17:10:32 +0530

    first commit

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ram').then(()=>
+mongoose.connect('mongodb+srv://ram:yTYBba4ejYmm5hTS@express.homstct.mongodb.net/').then(()=>
 {console.log('connected succsefully...')}).catch((error)=>{console.log(error)})
 
 
